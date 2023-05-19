ee755a7a27b4c351f2b30e0f099b37c374e8eaa6
marsrian <marsrian40@gmail.com>
2023-05-19 12:45:50 +0600

    react rating added

--- src/Pages/AllToys/ToyDetails.jsx
+++ src/Pages/AllToys/ToyDetails.jsx
@@ -1,9 +1,12 @@
 import { useLoaderData } from "react-router-dom";
 import { Rating } from '@smastrom/react-rating'
 import '@smastrom/react-rating/style.css'
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
 
 const ToyDetails = () => {
   const toy = useLoaderData();
+  const {user} = useContext(AuthContext);
   const {
     photo,
     toyName,
@@ -21,6 +24,7 @@ const ToyDetails = () => {
       <div className="border-2 border-gray-400 p-5 space-y-4 rounded-lg">
         <h3 className="text-xl font-semibold">Toy Name: {toyName}</h3>
         <p>Seller: {sellerName}</p>
+        <p>Seller email: {user?.email}</p>
         <p>SubCategory: {subCategory}</p>
         <p>Price: ${price}</p>
         <p className="flex items-center my-auto gap-2">Rating: <Rating className='' style={{ maxWidth: 150 }} value={rating} readOnly /></p>
